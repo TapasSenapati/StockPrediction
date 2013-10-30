802f21a3e57a64628152d4f40c6d5ed53b62d549
Tapas Kumar Senapati <tapassenapati89@yahoo.com>
2013-10-30 16:46:41 -0400

    Update twitter-search-between-dates.py
    fix twitter rate limit with sleep.

--- src/twitter-search-between-dates.py
+++ src/twitter-search-between-dates.py
@@ -1,8 +1,9 @@
 import tweepy
 import oauth2 as oauth
+import time
 
-auth = tweepy.auth.OAuthHandler('ihCnDKwZWZoXpeb7z3rQQ', 'TGcMdTGcxIQVC8dWeVrTNjjJhaU3GrsREE3U6jCqWc') #shayan's
-auth.set_access_token('1320599683-uTYgL1xAIWMUnIJlK9rKMFigukmoc6z5eGdnrDI', 'LW3oTRP69Qof08Act9iH13HzfN4ZOAzihxvvrUMWqI')
+auth = tweepy.auth.OAuthHandler('mwGjl1cDvr9wsZfSOKlrvw', '2UElrZPqrSaoUUHdmkq1XcngMYaBjJzSGs4lUW9h98I')
+auth.set_access_token('1437787351-xnDWc6myk4N4GQC2YK0u1EKDm89D0mfSzRIqmOa', 'dr6xwqJcnzcsv3be2HVS48pC12kzsemlDxXcZAXAY')
 api = tweepy.API(auth)
 # print api.me().name
 # for status in tweepy.Cursor(api.user_timeline,id='microsoft').items(15):
@@ -17,6 +18,7 @@ for tweet in tweepy.Cursor(api.search, q="Microsoft", rpp=10000,
                            lang=searchLanguage.encode("utf_8", "replace"),#"en",
                            with_twitter_user_id=True).items():
     print tweet.id, tweet.created_at#, tweet.text #tweet.user.name
+    time.sleep(0.5)
     store.append(tweet.user.name)
     print "\n"
      
